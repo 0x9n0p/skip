e45bd5e6272ac33f63b757fb1b58ba624fca368e
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2024-05-16 16:27:53 +0330

    Add unwrap to Error

--- error.go
+++ error.go
@@ -2,11 +2,16 @@ package skip
 
 type Error interface {
 	error
+	Unwrap() error
 	Skippable() bool
 }
 
 type ReturnError struct {
 	error
+}
+
+func (r *ReturnError) Unwrap() error {
+	return r.error
 }
 
 func (r *ReturnError) Skippable() bool {
